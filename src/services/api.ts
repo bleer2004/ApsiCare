4fc5850ccba15a9fb801b4b3d99cd09450713ac7
Verina Wadie <verinawadie1@gmail.com>
2026-07-26 22:19:21 -0300

    key fix

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,2 +1,2 @@
 export const API_URL = 'https://2ube699efh.execute-api.sa-east-1.amazonaws.com';
-export const GROQ_API_KEY = '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';
+export const GROQ_API_KEY = process.env.EXPO_PUBLIC_GROQ_API_KEY ?? '';
